981cdcd3654535f0c424dcc97e4ad89c64557fdb
Toxic-K-Ghost <kevincjay48@gmail.com>
2025-09-25 22:45:05 +0300

    Add files via upload

---
+++ program on Compound interest .c
@@ -0,0 +1,40 @@
+//simple compound interest program 
+/*
+Name:Kevin Kipngeno 
+Reg :PA106/G/28707/25
+Description :program to calculate compound interest and amount
+*/
+
+#include<stdio.h>
+#include <math.h>
+
+int main ()
+ {
+float principle;
+float rate;
+float time;
+int n;
+float amount ;
+
+//prompt the user to enter the principle 
+printf("enter principle: ") ;
+scanf("%f",&principle);
+
+//prompt the user to enter the value of the rate 
+printf("enter rate: ");
+scanf("%f", &rate );
+
+//prompt the user to enter time 
+printf("enter time: ");
+scanf("%f", &time );
+
+//prompt the user to enter value of n
+printf("enter n");
+scanf("%d", &n);
+
+//calculate the amount after compound interest 
+amount =principle*pow(1+rate/n, n*time) ;
+printf("amount is %f", amount );
+
+return 0 ;
+}
